2e9a499ee565428a0ee4dad8792226dbd5685dd9
papabn <pndiaye999@gmail.com>
2024-04-16 14:42:58 +0000

    add filtre on demandes page and offer pages

--- src/pages/Demandes.jsx
+++ src/pages/Demandes.jsx
@@ -9,39 +9,91 @@ import {
 } from "../components/ui/card";
 import useSWR from "swr";
 import { useUserStore } from "../store/UserStore";
+import { Button } from "../components/ui/button";
+import { fetcher } from "../utils/fertcher";
+import { useState } from "react";
 
 export default function Demandes() {
   const { user } = useUserStore();
-  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const [searchValue, setsearchValue] = useState();
+  const [alldemandes, setAlldemandes] = useState();
+
   const { data, error, isLoading } = useSWR(
     "http://localhost:8000/api/demandes",
     fetcher
   );
+
+
+  const handleSearchChange = (e) => {
+    setsearchValue(e.target.value);
+  };
   return (
-    <div className=" mt-16 mx-auto w-[95%] h-[100%] flex justify-center space-x-3 space-y-3 items-center flex-wrap text-white">
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error</p>}
-      {data &&
-        data?.demandes.map((demande) => (
-          <Card className="w-[30%] bg-gray-300 text-black shadow-lg">
-            <CardHeader>
-              <CardTitle>{demande?.titre}</CardTitle>
-              <CardDescription className="text-black font-semibold">
-                Publie par : {demande?.user_id === user?.id ? "Vous" : demande?.nomDemandeur} le{" "}
-                {new Date(demande?.created_at).toLocaleDateString('fr-FR', { day: 'numeric', month: 'long', year: 'numeric' })}
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>{demande?.description}</p>
-            </CardContent>
-            <CardFooter>
-              <p>
-                Date limite:{" "}
-                {new Date(demande?.date_limite).toLocaleDateString("fr-FR", { day: 'numeric', month: 'long', year: 'numeric' })}
-              </p>
-            </CardFooter>
-          </Card>
-        ))}     
-    </div>
+    <>
+      {/* barre de recherche */}
+      <div
+        className=" mt-20 flex justify-center m
+      t-5"
+      >
+        <input
+          type="text"
+          placeholder="Rechercher une demande"
+          className="w-[50%] h-10 rounded-lg p-2"
+          value={searchValue}
+          onChange={handleSearchChange}
+        />
+        
+      </div>
+      <div className=" mt-10 mx-auto w-[95%] h-[100%] flex justify-center space-x-3 space-y-3 items-center flex-wrap text-white">
+        {isLoading && <p>Loading...</p>}
+        {error && <p>Error</p>}
+        {data &&
+          data?.demandes
+            .filter((demande) =>
+              searchValue
+                ? demande.titre
+                    .toLowerCase()
+                    .includes(searchValue.toLowerCase()) ||
+                  demande.description
+                    .toLowerCase()
+                    .includes(searchValue.toLowerCase())
+                : true
+            )
+            .map((demande) => (
+              <Card className="w-[30%] bg-gray-300 text-black shadow-lg">
+                <CardHeader>
+                  <CardTitle>{demande?.titre}</CardTitle>
+                  <CardDescription className="text-black font-semibold">
+                    Publie par :{" "}
+                    {demande?.user_id === user?.id
+                      ? "Vous"
+                      : demande?.nomDemandeur}{" "}
+                    le{" "}
+                    {new Date(demande?.created_at).toLocaleDateString("fr-FR", {
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
+                    })}
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>{demande?.description}</p>
+                </CardContent>
+                <CardFooter>
+                  <p>
+                    Date limite:{" "}
+                    {new Date(demande?.date_limite).toLocaleDateString(
+                      "fr-FR",
+                      {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                      }
+                    )}
+                  </p>
+                </CardFooter>
+              </Card>
+            ))}
+      </div>
+    </>
   );
 }
--- src/pages/Offres.jsx
+++ src/pages/Offres.jsx
@@ -9,45 +9,78 @@ import {
   CardHeader,
   CardTitle,
 } from "../components/ui/card";
+import { useState } from 'react';
 export default function Offres() {
    const { user } = useUserStore();
+  const [searchValue, setsearchValue] = useState();
+
    const fetcher = (url) => fetch(url).then((res) => res.json());
    const { data, error, isLoading } = useSWR(
      "http://localhost:8000/api/services",
      fetcher
    );
+
+    const handleSearchChange = (e) => {
+      setsearchValue(e.target.value);
+    };
   return (
-    <div className="mt-16 p-3 mx-auto w-[95%] h-[100%] flex justify-center space-x-3  items-center flex-wrap text-white ">
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error</p>}
-      {data &&
-        data?.services.map((service) => (
-          <Card className="w-[30%] bg-gray-300 text-black shadow-lg">
-            <CardHeader>
-              <CardTitle>{service?.titre}</CardTitle>
-              <CardDescription className="text-black font-semibold">
-                Publie par :{" "}
-                {service?.user_id === user?.id
-                  ? "Vous"
-                  : service?.nomPrestataire}{" "}
-                le{" "}
-                {new Date(service?.created_at).toLocaleDateString("fr-FR", {
-                  day: "numeric",
-                  month: "long",
-                  year: "numeric",
-                })}
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <p>{service?.description}</p>
-              <p>tarif: {service?.tarif}/mois</p>
-              <p>Lieu: {service?.lieu}</p>
-            </CardContent>
-            <CardFooter>
-              <p>Contactez le Prestataire au: {service?.telephonePresta}</p>
-            </CardFooter>
-          </Card>
-        ))}
-    </div>
+    <>
+      {/* barre de recherche */}
+      <div
+        className=" mt-20 flex justify-center m
+      t-5"
+      >
+        <input
+          type="text"
+          placeholder="Rechercher une demande"
+          className="w-[50%] h-10 rounded-lg p-2"
+          value={searchValue}
+          onChange={handleSearchChange}
+        />
+      </div>
+      <div className="mt-10 p-3 mx-auto w-[95%] h-[100%] flex justify-center space-x-3  items-center flex-wrap text-white ">
+        {isLoading && <p>Loading...</p>}
+        {error && <p>Error</p>}
+        {data &&
+          data?.services
+            .filter((service) =>
+              searchValue
+                ? service.titre
+                    .toLowerCase()
+                    .includes(searchValue.toLowerCase()) ||
+                  service.description
+                    .toLowerCase()
+                    .includes(searchValue.toLowerCase())
+                : true
+            )
+            .map((service) => (
+              <Card className="w-[30%] bg-gray-300 text-black shadow-lg">
+                <CardHeader>
+                  <CardTitle>{service?.titre}</CardTitle>
+                  <CardDescription className="text-black font-semibold">
+                    Publie par :{" "}
+                    {service?.user_id === user?.id
+                      ? "Vous"
+                      : service?.nomPrestataire}{" "}
+                    le{" "}
+                    {new Date(service?.created_at).toLocaleDateString("fr-FR", {
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
+                    })}
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>{service?.description}</p>
+                  <p>tarif: {service?.tarif}/mois</p>
+                  <p>Lieu: {service?.lieu}</p>
+                </CardContent>
+                <CardFooter>
+                  <p>Contactez le Prestataire au: {service?.telephonePresta}</p>
+                </CardFooter>
+              </Card>
+            ))}
+      </div>
+    </>
   );
 }
